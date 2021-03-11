36383e1cdaa8799420d8f9eb971e81d478d09af6
franzedel <franz.t77@gmail.com>
2021-03-10 20:31:50 -0400

    Vistas Frontend y AdminLTE

--- resources/views/admin/layouts/pages/sidebar.blade.php
+++ resources/views/admin/layouts/pages/sidebar.blade.php
@@ -32,20 +32,6 @@
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
-                </a>
-              </li>
-            </ul>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
